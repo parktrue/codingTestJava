48c8bddf1000e4f9178508f14b6cc7c14fd44abd
jinhyeok <134375851+parktrue@users.noreply.github.com>
2023-06-04 17:20:30 +0900

    Update 문자열 출력하기.java

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,5 +4,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        int length = a.length();
+
+    if (length >= 1 && length <= 1000000) {
+      System.out.println(a);
     }
+  }
 }
