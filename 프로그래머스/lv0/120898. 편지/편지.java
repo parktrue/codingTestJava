f4641cb917da87dde89f6d941328b2313661adb1
jinhyeok <134375851+parktrue@users.noreply.github.com>
2023-06-11 16:04:32 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int len = message.length();
+        
+        answer = len * 2;
+        return answer;
+    }
+}
