0c1f6597b5582ac34a79626ee5926dfbf0358040
jinhyeok <134375851+parktrue@users.noreply.github.com>
2023-06-13 23:46:36 +0900

    [unrated] Title: 문자 개수 세기, Time: 1.52 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] solution(String my_string) {
+        char[] az = new char[26];
+        char[] AZ = new char[26];
+        
+        int a = 97;
+        int A = 65;
+        for (int i = 0; i < 26; i++) {
+            az[i] = (char) a;
+            a++;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            AZ[i] = (char) A;
+            A++;
+        }
+        
+        String azStr = new String(az);
+        String AZStr = new String(AZ);
+        String atoZ = AZStr + azStr;
+        
+        int len = my_string.length();
+        int[] answer = new int[52];
+        char[] atoZChar = atoZ.toCharArray();
+        char[] myChar = my_string.toCharArray();
+        for (int i = 0; i < 52; i++) {
+            for (int j = 0; j < len; j++) {
+                if (atoZChar[i] == myChar[j]) {
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
