9be4347dd67d5e3e35c1f3febe2b2773b02140e1
jinhyeok <134375851+parktrue@users.noreply.github.com>
2023-06-15 17:53:05 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        int len = arr.length;
+        
+        for (int i = idx; i < len; i++) {
+            if (arr[i] == 1) {
+                answer = i;
+                return answer;
+            }
+        }
+        answer = -1;
+        return answer;
+    }
+}
