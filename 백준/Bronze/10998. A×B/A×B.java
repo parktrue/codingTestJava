0f0e2a71525c4a1bca442064971eb7ef6f86a7af
jinhyeok <134375851+parktrue@users.noreply.github.com>
2023-06-25 21:41:12 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int result = 0;
+        result = A * B;
+        System.out.print(result);
+        sc.close();
+    }
+}
